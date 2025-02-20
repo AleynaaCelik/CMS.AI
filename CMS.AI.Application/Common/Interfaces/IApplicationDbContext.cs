8321f4313879e6f23dbb397c0549498cfb3e5edd
Aleyna ÇELİK <77541289+AleynaaCelik@users.noreply.github.com>
2025-02-20 23:06:00 +0300

    Interfaces Added

---
+++ CMS.AI.Application/Common/Interfaces/IApplicationDbContext.cs
@@ -0,0 +1,19 @@
+﻿using CMS.AI.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CMS.AI.Application.Common.Interfaces
+{
+    public interface IApplicationDbContext
+    {
+        DbSet<Content> Contents { get; }
+        DbSet<ContentMeta> ContentMetas { get; }
+        DbSet<ContentVersion> ContentVersions { get; }
+        Task<int> SaveChangesAsync(CancellationToken cancellationToken);
+    }
+}
