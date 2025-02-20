c1c479b3027730b3cf9efe6d718b045ba11780e9
Aleyna ÇELİK <77541289+AleynaaCelik@users.noreply.github.com>
2025-02-20 22:51:30 +0300

    Events Added

---
+++ CMS.AI.Domain/Events/ContentCreatedEvent .cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CMS.AI.Domain.Events
+{
+    public class ContentCreatedEvent : DomainEvent
+    {
+        public Guid ContentId { get; }
+
+        public ContentCreatedEvent(Guid contentId)
+        {
+            ContentId = contentId;
+        }
+    }
+}
---
+++ CMS.AI.Domain/Events/ContentUpdatedEvent .cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CMS.AI.Domain.Events
+{
+    public class ContentUpdatedEvent : DomainEvent
+    {
+        public Guid ContentId { get; }
+        public int Version { get; }
+
+        public ContentUpdatedEvent(Guid contentId, int version)
+        {
+            ContentId = contentId;
+            Version = version;
+        }
+    }
+}
