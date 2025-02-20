7c583b6d506cd6363d4edd09a98c5f25c911092c
Aleyna ÇELİK <77541289+AleynaaCelik@users.noreply.github.com>
2025-02-20 22:43:56 +0300

    Commons Added

---
+++ CMS.AI.Domain/Common/BaseEntity.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CMS.AI.Domain.Common
+{
+    public abstract class BaseEntity
+    {
+        public Guid Id { get; protected set; }
+        public DateTime CreatedAt { get; set; }
+        public string CreatedBy { get; set; }
+        public DateTime? LastModifiedAt { get; set; }
+        public string LastModifiedBy { get; set; }
+    }
+
+}
