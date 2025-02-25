0b06e9c07fef44a39e1641f7e9d3a60a64045e14
Aleyna ÇELİK <77541289+AleynaaCelik@users.noreply.github.com>
2025-02-25 22:46:20 +0300

    Synchronized Application Project File

---
+++ CMS.AI.Application/DependencyInjection.cs
@@ -0,0 +1,23 @@
+﻿using System.Reflection;
+using Microsoft.Extensions.DependencyInjection;
+using FluentValidation;
+
+namespace CMS.AI.Application
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddApplication(this IServiceCollection services)
+        {
+            var assembly = Assembly.GetExecutingAssembly();
+
+            // MediatR
+            services.AddMediatR(cfg => {
+                cfg.RegisterServicesFromAssembly(assembly);
+            });
+
+            // Diğer servisler burada kaydedilebilir
+
+            return services;
+        }
+    }
+}
