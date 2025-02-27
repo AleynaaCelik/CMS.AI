0c73a45d2cd98089439485886d948814b0709011
Aleyna ÇELİK <77541289+AleynaaCelik@users.noreply.github.com>
2025-02-28 00:02:55 +0300

    Search Service Interface

---
+++ CMS.AI.Application/Common/Interfaces/ISearchService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CMS.AI.Application.Common.Interfaces
+{
+    public interface ISearchService<T> where T : class
+    {
+        Task<IReadOnlyCollection<T>> SearchAsync(string query, int page = 1, int pageSize = 10, CancellationToken cancellationToken = default);
+        Task IndexDocumentAsync(T document, CancellationToken cancellationToken = default);
+        Task UpdateDocumentAsync(T document, CancellationToken cancellationToken = default);
+        Task DeleteDocumentAsync(string id, CancellationToken cancellationToken = default);
+    }
+
+
+}
